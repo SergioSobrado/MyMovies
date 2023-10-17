7f03179cb24094e1817cf6bf83fbb9400456826a
Ubuntu <ubuntu@ip-172-31-56-105.ec2.internal>
2023-10-17 21:54:40 +0000

    Add Static Function

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from movies.models import Movie
 
 # Create your views here.
 def index(request):
-    return render(request, './index.html')
+    movies = Movie.objects.all()
+    context = {'movie_list': movies}
+    return render(request, './index.html', context)
+
+def movies(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    context = {'movie': movie}
+    return render(request, './movie.html', context)
